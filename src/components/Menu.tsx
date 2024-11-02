dd1de5a9462745ec1890505f396410e01ca08cf0
yZanni <164171833+JoaoPedroCZanni@users.noreply.github.com>
2024-11-02 15:59:44 -0300

    Implementação do Menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Menu(){
+    return(
+        <nav className="flex">
+            <ul className="flex gap-6 links">
+                <li><Link href={'/'}>Home</Link></li>
+                <li><Link href={'/'}>Cadastro</Link></li>
+                <li><Link href={'/'}>Serviços</Link></li>
+                <li><Link href={'/'}>Página de Integrantes</Link></li>
+                <li><Link href={'/'}>Contato</Link></li>
+                
+                
+            </ul>
+        </nav>
+    )
+} 
